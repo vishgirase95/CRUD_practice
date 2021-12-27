ec3377c9ade9f90be25e3ed86633f150a26a86c3
vish <vishgirase95@gmail.com>
2021-12-27 22:42:23 +0530

    [vishal] added all prac

--- javascript-prac/array.js
+++ javascript-prac/array.js
@@ -1,1 +1,98 @@
-let rank=[1,2,3,4]
+// // let fruits = ['Apple', 'Banana'];
+
+// // console.log(fruits.length);
+
+
+// let fruits = ['Apple', 'Banana']
+
+// console.log(fruits.length)
+
+// let first = fruits[1]
+// console.log(first);
+
+// fruits.push("orange");
+// fruits.push("orange","kiwi");
+// console.log(fruits);
+
+// fruits.pop();
+// console.log(fruits);
+
+
+// fruits.shift();
+// console.log(fruits);
+
+// fruits.unshift("apple");
+// console.log(fruits);
+
+
+// let index=fruits.indexOf("apple");
+// console.log(index);
+
+// fruits.splice(2,0,"table","chair");
+// console.log(fruits);
+
+
+// console.log(fruits.length);
+
+// console.log("sort"+fruits.sort());
+
+
+// fruits.splice(2,2);
+// console.log(fruits);
+
+// let a="hello vishal";
+// // console.log(a.slice(3,5));
+// let b=a.split(" ");
+// console.log(b[1]);
+
+
+// let join=["a","apple","ball","cat"];
+// let done=join.join(":");
+// console.log(done);
+
+// let string=join.toString();
+// console.log(string);
+
+// let first=["apple","banna"];
+// let second=["cat","dog"];
+// let third=["elephant","lion"];
+// const two=first.concat(second,third);
+// console.log(two);
+
+
+// higher order
+// const greeting=(name)=>{
+//     console.log("hi");
+//     return ()=>{
+//         console.log("hello"+name);
+//     }
+// }
+
+
+// greeting("vishal")();
+
+
+
+// closer
+
+// const closer=()=>{
+//     let a=5;
+//     let abc =()=>{
+//         console.log(a);
+//     }
+//     return abc();
+// }
+
+// closer();
+
+
+// let obj={
+//     name:"vishal",
+//     isBool: true,
+//     arr1:[1,2,3,4],
+//     "fruit":"apple"
+// }
+
+// console.log(obj);
+// console.log(obj.name);
+// console.log(obj.fruit);
